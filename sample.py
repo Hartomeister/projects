268c92f6930f301f33dd0b2d873c3ee3c6504189
Hartomeister <64258322+Hartomeister@users.noreply.github.com>
2022-08-04 00:41:19 +0300

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import telebot
+import logging
+logger = logging.getLogger("mylogger")
+streamHandler = logging.StreamHandler()
+streamHandler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+streamHandler.setFormatter(formatter)
+
+logger.addHandler(streamHandler)
 
 bot = telebot.TeleBot('YOUR TOKEN')
 
